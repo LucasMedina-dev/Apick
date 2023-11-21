b3653f31c55286a3a3785421fd1ff2ca0dda3345
Lucas <medinalucasian@gmail.com>
2023-11-21 20:04:47 -0300

    testing methods developed

--- src/app/custom-endpoint/custom-endpoint.component.ts
+++ src/app/custom-endpoint/custom-endpoint.component.ts
@@ -11,25 +11,39 @@ import { ApimanagerService } from '../apimanager.service';
 export class CustomEndpointComponent implements OnChanges {
   @Input() customizerData!: CustomizerStruct;
   @Input() endpointData: any;
-  constructor(private apiManager : ApimanagerService) {}
+  documents !: any;
+  constructor(private apiManager: ApimanagerService) {}
   formCustomizer = new FormGroup({
     queryParameters: new FormControl(false, Validators.required),
     limitDocuments: new FormControl(0, Validators.required),
     randomResponse: new FormControl(false, Validators.required),
   });
-  saveCustomData(){
-    this.apiManager.saveCustomizerData(this.customizerData, this.formCustomizer.value).subscribe({
-      next: (response)=>{
-        console.log(response)
+  saveCustomData() {
+    this.apiManager
+      .saveCustomizerData(this.customizerData, this.formCustomizer.value)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+      });
+  }
+  launchTest() {
+    let idEndpoint = this.customizerData.idEndpoint;
+    //let method = this.customizerData.method;
+    let url = `http://localhost:3000/api/endpoint/${idEndpoint}`;
+    this.apiManager.getDocs(url).subscribe({
+      next: (data)=> {
+        this.documents=data;
       }
     })
   }
   ngOnChanges() {
+    this.documents=Object;
     if (this.customizerData) {
       this.formCustomizer.patchValue({
         queryParameters: this.customizerData.queryParameters,
         limitDocuments: this.customizerData.limitDocuments,
-        randomResponse: this.customizerData.randomResponse
+        randomResponse: this.customizerData.randomResponse,
       });
     }
   }
